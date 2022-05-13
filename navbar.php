36fb317a5c38ba420157de00a9e14732977e182f
Angel Oribiada <oribiada24@gmail.com>
2022-05-13 16:38:06 +0800

    update2

--- navbar.php
+++ navbar.php
@@ -1,34 +1,104 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Products<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Best Seller</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                    About
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
+		<div class="wrap">
+			<div class="container">
+				<div class="row justify-content-between">
+						<div class="col">
+							<p class="mb-0 phone"><span class="fa fa-phone"></span> <a href="#">+00 1234 567</a></p>
+						</div>
+						<div class="col d-flex justify-content-end">
+							<div class="social-media">
+				    		<p class="mb-0 d-flex">
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
+				    		</p>
+			            </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light sticky-top" id="ftco-navbar">
+	    	<div class="container">
+	    		<a class="navbar-brand" href="index.html">Papermag <span>Magazine</span></a>
+	    		<form action="#" class="searchform order-sm-start order-lg-last ml-auto">
+					<div class="form-group d-flex">
+						<input type="text" class="form-control pl-3" placeholder="Search">
+						<button type="submit" placeholder="" class="form-control search"><span class="fa fa-search"></span></button>
+					</div>
+        		</form>
+	      		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        		<span class="fa fa-bars"></span> Menu
+	      		</button>
+	      		<div class="collapse navbar-collapse" id="ftco-nav">
+					<ul class="navbar-nav m-auto">
+						<li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Page</a>
+							<div class="dropdown-menu" aria-labelledby="dropdown04">
+								<a class="dropdown-item" href="#">Page 1</a>
+								<a class="dropdown-item" href="#">Page 2</a>
+								<a class="dropdown-item" href="#">Page 3</a>
+								<a class="dropdown-item" href="#">Page 4</a>
+							</div>
+						</li>
+	        			<li class="nav-item"><a href="#" class="nav-link">Catalog</a></li>
+	        			<li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
+	          			<li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+	        		</ul>
+	      		</div>
+	    	</div>
+	  	</nav>
+    <!-- END nav -->
+
+
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
     </div>
-</nav>
