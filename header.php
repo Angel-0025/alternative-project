15ef5094f0dea285d9f06f23822e5fd50586da33
Angel Oribiada <oribiada24@gmail.com>
2022-05-14 22:05:21 +0800

    update 1.1

--- header.php
+++ header.php
@@ -2,8 +2,20 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
+<!---->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!---->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <!-- Bootstrap 4 JavaScript-->
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+<!-- Slick Js-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- Owl Carousel-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="script.js"></script>
--- navbar.php
+++ navbar.php
@@ -1,104 +1,174 @@
-
-		<div class="wrap">
-			<div class="container">
-				<div class="row justify-content-between">
-						<div class="col">
-							<p class="mb-0 phone"><span class="fa fa-phone"></span> <a href="#">+00 1234 567</a></p>
+<div class="wrap">
+	<div class="container">
+		<div class="row justify-content-between">
+			<div class="col">
+				<p class="mb-0 phone"><span class="fa fa-phone"></span> <a href="#">+00 1234 567</a></p>
+			</div>
+			<div class="col d-flex justify-content-end">
+				<div class="social-media">
+			    	<p class="mb-0 d-flex">
+			    		<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+			    		<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+			    		<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+						<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
+			    	</p>
+		        </div>
+			</div>
+		</div>
+	</div>
+</div>
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light sticky-top" id="ftco-navbar">
+	<div class="container">
+	    <a class="navbar-brand" href="index.html">Papermag <span>Magazine</span></a>
+	    <form action="#" class="searchform order-sm-start order-lg-last ml-auto">
+			<div class="form-group d-flex">
+				<input type="text" class="form-control pl-3" placeholder="Search">
+				<button type="submit" placeholder="" class="form-control search"><span class="fa fa-search"></span></button>
+			</div>
+        </form>
+	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="fa fa-bars"></span> Menu
+		</button>
+	    <div class="collapse navbar-collapse" id="ftco-nav">
+			<ul class="navbar-nav m-auto">
+				<li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Page</a>
+					<div class="dropdown-menu" aria-labelledby="dropdown04">
+						<a class="dropdown-item" href="#">Page 1</a>
+						<a class="dropdown-item" href="#">Page 2</a>
+						<a class="dropdown-item" href="#">Page 3</a>
+						<a class="dropdown-item" href="#">Page 4</a>
+					</div>
+				</li>
+	    		<li class="nav-item"><a href="#" class="nav-link">Catalog</a></li>
+	        	<li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
+	    		<li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+    		</ul>
+	    </div>
+	</div>
+</nav>
+    <!-- END nav -->
+<div class="main-content">
+	<div class="section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6">
+					<div class="heading_s1 text-center">
+						<h2>Featured Products</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="product_slider carousel_slider owl-carousel owl-theme nav_style1 owl-loaded owl-drag">
+						<div class="owl-stage-outer">
+							<div class="owl-stage" style="transform: translate3d(-1130px, 0px, 0px); transition: all 0s ease 0s; width: 3673px;">
+							</div>
 						</div>
-						<div class="col d-flex justify-content-end">
-							<div class="social-media">
-				    		<p class="mb-0 d-flex">
-				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
-				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
-				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
-				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
-				    		</p>
-			            </div>
+						<div class="item">
+							<div class="product">
+								<div class="product-img">
+									<a href="shop-product-detail.html">
+										<img src="https://media.istockphoto.com/photos/programming-source-code-abstract-background-picture-id1047259374?k=20&m=1047259374&s=612x612&w=0&h=pt3XbBvrmiYgoYmVzsaUeXtV8vw_jJI9Ly8P7AL6Qig=" alt="product_img1">
+									</a>
+									<div class="product_action_box">
+										<ul class="list_none pr_action_btn">
+											<li class="add-to-cart"><a href="#"><i class="icon-basket-loaded"></i> Add To Cart</a></li>
+											<li><a href="shop-compare.html" class="popup-ajax"><i class="icon-shuffle"></i></a></li>
+											<li><a href="shop-quick-view.html" class="popup-ajax"><i class="icon-magnifier-add"></i></a></li>
+											<li><a href="#"><i class="icon-heart"></i></a></li>
+										</ul>
+									</div>	
+								</div>
+								<div class="product_info">
+									<h6 class="product_title"><a href="shop-product-detail.html">Blue Dress For Woman</a></h6>
+									<div class="product_price">
+                                    <span class="price">$45.00</span>
+										<del>$55.25</del>
+										<div class="on_sale">
+											<span>35% Off</span>
+										</div>
+										<div class="rating_wrap">
+											<div class="rating">
+												<div class="product_rate" style="width:80%"></div>
+											</div>
+											<span class="rating_num">(21)</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light sticky-top" id="ftco-navbar">
-	    	<div class="container">
-	    		<a class="navbar-brand" href="index.html">Papermag <span>Magazine</span></a>
-	    		<form action="#" class="searchform order-sm-start order-lg-last ml-auto">
-					<div class="form-group d-flex">
-						<input type="text" class="form-control pl-3" placeholder="Search">
-						<button type="submit" placeholder="" class="form-control search"><span class="fa fa-search"></span></button>
-					</div>
-        		</form>
-	      		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        		<span class="fa fa-bars"></span> Menu
-	      		</button>
-	      		<div class="collapse navbar-collapse" id="ftco-nav">
-					<ul class="navbar-nav m-auto">
-						<li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Page</a>
-							<div class="dropdown-menu" aria-labelledby="dropdown04">
-								<a class="dropdown-item" href="#">Page 1</a>
-								<a class="dropdown-item" href="#">Page 2</a>
-								<a class="dropdown-item" href="#">Page 3</a>
-								<a class="dropdown-item" href="#">Page 4</a>
-							</div>
-						</li>
-	        			<li class="nav-item"><a href="#" class="nav-link">Catalog</a></li>
-	        			<li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
-	          			<li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-	        		</ul>
-	      		</div>
-	    	</div>
-	  	</nav>
-    <!-- END nav -->
+	</div>
+</div>
 
+<script>
 
-    <div class="container">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum minima velit ullam suscipit minus laboriosam facere pariatur earum quis, libero voluptatibus quasi possimus fuga voluptate quam nostrum inventore veniam. Unde aliquid fugiat libero dolores ipsam rerum repudiandae dolore perspiciatis esse. Eius, veniam earum. Quaerat sed adipisci doloribus dolores quisquam delectus.</p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-    </div>
+$('.slick-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});	
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+	dots: false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        481:{
+            items:2,
+            nav:true
+        },
+        768:{
+            items:4,
+            nav:true,
+            loop:true
+        },
+		1199:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+})
+</script>
+    
