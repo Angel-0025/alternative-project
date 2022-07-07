64ccc5e1e40e6dede2419623ef8a1b32fb28dbfb
Angel Oribiada <oribiada24@gmail.com>
2022-07-07 23:35:01 +0800

    update start tom admin tables

--- admin_panel/dashboard.php
+++ admin_panel/dashboard.php
@@ -61,19 +61,22 @@
           <table id="table_id" class="display">
               <thead>
                   <tr>
-                      <th>Column 1</th>
-                      <th>Column 2</th>
+                      <th>Count</th>
+                      <th>Reference Num</th>
+                      <th>Customer Name</th>
+                      <th>Amount</th>
+                      <th>Date</th>
                   </tr>
               </thead>
               <tbody>
                   <tr>
                       <td>Row 1 Data 1</td>
                       <td>Row 1 Data 2</td>
+                      <td>Row 1 Data 1</td>
+                      <td>Row 1 Data 2</td>
+                      <td>Row 1 Data 1</td>
                   </tr>
-                  <tr>
-                      <td>Row 2 Data 1</td>
-                      <td>Row 2 Data 2</td>
-                  </tr>
+                  
               </tbody>
           </table>
         </div>
